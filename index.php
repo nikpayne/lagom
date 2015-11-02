cf76b34b9f1d4a0656ade209cb232390fb89ab6b
Nik <nikolasapayne@gmail.com>
2015-11-02 09:38:26 -0800

    updating

--- index.php
+++ index.php
@@ -83,17 +83,17 @@
     $('.button a').on('click',function(){
       if(open == false){
         $('.overlay, .btn-close').fadeIn(200);
-        $('nav ul li, .btn-open').fadeOut(200);
+        $('nav ul li, .btn-open, .array').fadeOut(200);
         open = true;
       } else {
         $('.overlay, .btn-close').fadeOut(200)
-        $('nav ul li, .btn-open').fadeIn(200);
+        $('nav ul li, .btn-open, .array').fadeIn(200);
         open = false;
       }
     });
     $('.overlay').on('mouseup', function(){
       $('.overlay, .btn-close').fadeOut(200);
-      $('nav ul li, .btn-open').fadeIn(200);
+      $('nav ul li, .btn-open, .array').fadeIn(200);
       open = false;
     });
     $('.wrap').on('mouseup', function(){
